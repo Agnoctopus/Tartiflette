a3c65444c4ef97a784bd9b6ba818ae5ef308619a
Tanguy Dubroca <tanguy.dubroca@lse.epita.fr>
2021-10-07 11:47:27 +0200

    fuzzers(giflib): implement syscall emulation for the target

--- fuzzers/giflib/src/sysemu.rs
+++ fuzzers/giflib/src/sysemu.rs
@@ -1,4 +1,5 @@
 use tartiflette_vm::{Vm, Register};
+use std::convert::From;
 
 /// Linux syscall emulation state
 pub struct SysEmu {
@@ -8,6 +9,27 @@ pub struct SysEmu {
     mmap_end: u64,
     /// Current address in the mmap are
     mmap_current: u64
+}
+
+/// Supported linux syscalls
+enum Syscall {
+    Mmap,
+    Munmap,
+    Ioctl,
+    ExitGroup,
+    Unknown
+}
+
+impl From<u64> for Syscall {
+    fn from(value: u64) -> Self {
+        match value {
+            9 => Syscall::Mmap,
+            11 => Syscall::Munmap,
+            16 => Syscall::Ioctl,
+            231 => Syscall::ExitGroup,
+            _ => Syscall::Unknown
+        }
+    }
 }
 
 impl SysEmu {
@@ -23,8 +45,53 @@ impl SysEmu {
     /// Handles a syscall. Returns whether execution should continue
     pub fn syscall(&mut self, vm: &mut Vm) -> bool {
         let syscall_code = vm.get_reg(Register::Rax);
-        println!("syscall[{}] exiting", syscall_code);
-        false
+
+        let result = match syscall_code.into() {
+            Syscall::Mmap => {
+                let addr = vm.get_reg(Register::Rdi);
+                let len = vm.get_reg(Register::Rsi);
+                let fd = vm.get_reg(Register::R8) as i64;
+
+                if fd != -1 {
+                    panic!("mmaping from a fd is not supported");
+                }
+
+                if len & 0xff != 0 {
+                    panic!("len is not aligned");
+                }
+
+                if addr != 0 {
+                    panic!("Mapping to a fixed address (0x{:x}) is not supported", addr);
+                }
+
+                if self.mmap_current + len > self.mmap_end {
+                    panic!("Mmap allocator out of memory");
+                }
+
+                vm.set_reg(Register::Rax, self.mmap_current);
+                self.mmap_current += len;
+                true
+            }
+            Syscall::Munmap => {
+                // Do a nop
+                vm.set_reg(Register::Rax, 0);
+                true
+            }
+            Syscall::Ioctl => {
+                // ioctl(1, TIOCGWINSZ, {ws_row=58, ws_col=239, ws_xpixel=0, ws_ypixel=0}) = 0
+                vm.set_reg(Register::Rax, 0);
+                true
+            }
+            Syscall::ExitGroup => {
+                // Simply stop the execution
+                false
+            }
+            Syscall::Unknown => {
+                panic!("Unhandled syscall: {}", syscall_code);
+            }
+        };
+
+        result
     }
 
     /// Resets the internal state of emulation layer
